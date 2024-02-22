21d8785c78e8b757ae94ac0e1fa92d21e5f40912
Arshad Ali <aalee9263@gmail.com>
2024-02-22 23:31:11 +0500

    final

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,11 +1,30 @@
 const addbtn = document.querySelector("#addbtn")
 const main = document.querySelector("#main")
+const saveNotes = () => {
+    const notes = document.querySelectorAll(".note textarea");
+    console.log(notes)
+    const data = [];
+    notes.forEach(
+        (note) => {
+            data.push(note.value)
+        }
+    )
+    // console.log(data)
+if (data.length === 0) {
+    localStorage.removeItem("notes")
+}   else {
+    localStorage.setItem("notes", JSON.stringify(data))
+}
+
+    
+}
+
 addbtn.addEventListener(
     "click",
     function(){
         addNote()
     }
-)
+);
 
 // <div class="note">
 //             <div class="tool">
@@ -15,16 +34,53 @@ addbtn.addEventListener(
 //             <textarea ></textarea>
 //         </div>
 
-const addNote = () => {
+const addNote = (text = "") => {
     const note =  document.createElement("div");
     note.classList.add("note")
     note.innerHTML = `
             <div class="tool">
-                <i class="fa-solid fa-floppy-disk"></i>
-                <i class="fa-solid fa-trash"></i>  
+                <i class="save fa-solid fa-floppy-disk"></i>
+                <i class="trash fa-solid fa-trash"></i>  
             </div>
-            <textarea ></textarea>
+            <textarea>${text}</textarea>
     `;
 
+    note.querySelector(".trash").addEventListener(
+        "click",
+        function(){
+            note.remove()
+            saveNotes()
+        }
+    )
+
+    note.querySelector(".save").addEventListener(
+        "click",
+        function(){
+            saveNotes()
+        }
+    )
+    note.querySelector("textarea").addEventListener(
+        "focusout",
+        function(){
+            saveNotes()
+        }
+    )
     main.appendChild(note)
+    saveNotes()
 }
+
+(
+    function(){
+        const lsNotes = JSON.parse(localStorage.getItem("notes"))
+        if (lsNotes === null) {
+            addNote()
+        } else {
+            lsNotes.forEach(
+                (lsNotes) =>{
+                    addNote(lsNotes)
+                }
+            )
+        }
+        
+     }
+)()
